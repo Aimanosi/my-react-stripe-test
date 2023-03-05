e59f210edd7a11f2175170cda3f4406682ec8a53
nasiru hammed aimanosi <nasaces360@gmail.com>
2023-03-05 21:51:27 +0100

    updated pay

--- src/Pay.jsx
+++ src/Pay.jsx
@@ -1,12 +1,35 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import StripeCheckout from "react-stripe-checkout";
 
 const KEY =
   "pk_test_51MhFfMGuebOPsVmpH9PdfoxpuyIEMBVW3UutTyYFELMY5Vh2EQ3fRC2iVtzKvH5zx1vHEjewqmCTmqZXHnAb4JHY00DS8FoISb";
 
 const Pay = () => {
+  const [stripeToken, setStripeToken] = useState(null);
+
   const onToken = (token) => {
     console.log(token);
+    setStripeToken(token);
   };
+
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        const res = await axios.post(
+          "http://localhost:5000/api/checkout/payment",
+          {
+            tokenId: stripeToken.id,
+            amount: 2000,
+          }
+        );
+        console.log(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    stripeToken && makeRequest();
+  }, [stripeToken]);
 
   return (
     <div
